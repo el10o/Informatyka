305a05e008df22a113b1880441744908e6863e86
el10o <emmbajer@gmail.com>
2023-11-08 14:09:02 +0100

    Komentarz

---
+++ lekcja 3 pentle/pentle.cpp
@@ -0,0 +1,40 @@
+ #include <iostream>
+ using namespace std;
+ 
+ 	int main(){
+ 		/* Inkrementacja
+ 		1 = 1 + 1
+ 		1 += 1
+ 		1++
+ 		Dekrementacja
+ 		1 = 1 - 1
+ 		1 -= 1
+ 		1--
+ 		
+ 		for(wartosc poczatkowa; warunek konczacy; krok)
+ 		
+ 		*/
+ 		
+ 		for(int i = 0; i < 5; i+=2){
+ 			cout << i << endl;
+		 }
+ 		
+ 		
+ 		//pentla zagniezdzona
+ 		for(int i = 0; i < 5; i++){
+ 			for(int j = 0; j < 7; j++){
+ 					cout << "*";
+ 					
+			 }
+			 cout << endl;
+		 }
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+	 }

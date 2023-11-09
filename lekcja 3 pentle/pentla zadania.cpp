761f407dad2d88dfa5ff2cad903d6165b98fb927
el10o <emmbajer@gmail.com>
2023-11-09 15:03:47 +0100

    Komentarz

---
+++ lekcja 3 pentle/pentla zadania.cpp
@@ -0,0 +1,12 @@
+  #include <iostream>
+ using namespace std;
+ 
+ 	int main(){ 
+    for(int i=0; i < 11; i++){
+       for(int j=0; j < 11; j++){
+	   cout << endl;
+           cout << i << " * " << j << "=" << i*j << endl;
+       }
+       cout << endl;
+   }
+}

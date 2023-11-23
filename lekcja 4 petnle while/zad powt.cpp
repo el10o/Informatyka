d1da82d9364d54fecdbe3878b742629f7f2352be
el10o <emmbajer@gmail.com>
2023-11-23 15:02:48 +0100

    Komentarz

---
+++ lekcja 4 petnle while/zad powt.cpp
@@ -0,0 +1,19 @@
+  #include <iostream>
+using namespace std;
+
+int main(){
+	short a = 30;
+	while( a >=10 ){
+		cout << a << ", ";
+		a -= 2;
+	}
+	cout << endl;
+	
+	short b;
+	cout << "podaj liczbe";
+	cin >> b;
+	do{
+		cout << b << ", ";
+		b /= 2;
+	}while(b > 0);
+}
